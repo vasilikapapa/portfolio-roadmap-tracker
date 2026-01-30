b38af2070de202263a11206eaadcb14a19ad8b3a
Vasilika Papa <vasilikapp@gmail.com>
2026-01-30 13:50:37 -0600

    Initial Spring Boot project (Initializr)

---
+++ portfolio-tracker/src/main/java/com/vasilika/portfoliotracker/PortfoliotrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vasilika.portfoliotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliotrackerApplication.class, args);
+	}
+
+}
